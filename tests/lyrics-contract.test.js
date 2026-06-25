dff07883213640b6512f1fc94b807a2b3a4dc94c
Arthur Lambillotte <github@artlambi.be>
2026-06-25 13:31:43 +0200

    Try regular lyrics weight with smoothing

--- tests/lyrics-contract.test.js
+++ tests/lyrics-contract.test.js
@@ -115,7 +115,7 @@ test('lyrics words stagger animate in when the overlay opens', () => {
   assert.match(html, /@keyframes lyricWordIn/);
   assert.match(html, /<link rel="stylesheet" href="https:\/\/use\.typekit\.net\/qsr7tur\.css">/);
   assert.match(html, /\.lyrics-line[\s\S]*color:\s*rgba\(255, 255, 255, 0\.82\)/);
-  assert.match(html, /\.lyrics-line[\s\S]*font:\s*300 20px\/1\.18 "mendl-serif-dawn", sans-serif/);
+  assert.match(html, /\.lyrics-line[\s\S]*font:\s*400 20px\/1\.18 "mendl-serif-dawn", sans-serif/);
   assert.match(html, /\.lyrics-line[\s\S]*font-style:\s*normal/);
   assert.match(html, /\.lyrics-line[\s\S]*-webkit-font-smoothing:\s*antialiased/);
   assert.match(html, /\.lyrics-line[\s\S]*-moz-osx-font-smoothing:\s*grayscale/);
