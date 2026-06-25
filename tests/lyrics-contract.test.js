32718f7d74595abe974dca793c4a8e3097c2448f
Arthur Lambillotte <github@artlambi.be>
2026-06-25 14:47:19 +0200

    Optically center control icons

--- tests/lyrics-contract.test.js
+++ tests/lyrics-contract.test.js
@@ -360,6 +360,7 @@ test('mute control hooks are preserved', () => {
   assert.match(html, /class="icon icon-off"/);
   assert.match(html, /data-sketch-icon="volume-on"/);
   assert.match(html, /data-sketch-icon="volume-off"/);
+  assert.match(html, /data-sketch-icon="volume-off" viewBox="-0\.75 0 24 24"/);
   assert.match(html, /\.mute-btn\.is-muted \.icon-on/);
   assert.match(html, /\.mute-btn\.is-muted \.icon-off/);
 });
@@ -393,8 +394,8 @@ test('lyrics control uses normal and selected Sketch icons', () => {
   assert.match(html, /data-sketch-icon="lyrics-selected"/);
   assert.match(html, /class="icon icon-lyrics"/);
   assert.match(html, /class="icon icon-lyrics-selected"/);
-  assert.match(html, /data-sketch-icon="lyrics" viewBox="-0\.5 0 24 24"/);
-  assert.match(html, /data-sketch-icon="lyrics-selected" viewBox="-0\.5 0 24 24"/);
+  assert.match(html, /data-sketch-icon="lyrics" viewBox="-1\.25 -0\.75 24 24"/);
+  assert.match(html, /data-sketch-icon="lyrics-selected" viewBox="-1\.25 -0\.75 24 24"/);
   assert.match(html, /\.lyrics-btn \.icon-lyrics-selected[\s\S]*opacity:\s*0/);
   assert.match(html, /\.lyrics-btn\.is-active \.icon-lyrics[\s\S]*opacity:\s*0/);
   assert.match(html, /\.lyrics-btn\.is-active \.icon-lyrics-selected[\s\S]*opacity:\s*1/);
